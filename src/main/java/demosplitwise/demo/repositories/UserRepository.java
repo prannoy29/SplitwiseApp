1e81eea2fa0a0522df0c34e57ea747090c18a6ac
Srinivas Kotla <srinivas.kotla@zemosolabs.com>
2017-08-17 16:50:14 +0530

    Changing output of method to list of Users from list of usernames

--- src/main/java/demosplitwise/demo/repositories/UserRepository.java
+++ src/main/java/demosplitwise/demo/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT name FROM User u where u.name LIKE concat(:username,'%') ")
-    List<String> matchedNames(@Param("username") String username);
+    @Query("SELECT u FROM User u where u.name LIKE concat(:username,'%') ")
+    List<User> matchedNames(@Param("username") String username);
 
 }
