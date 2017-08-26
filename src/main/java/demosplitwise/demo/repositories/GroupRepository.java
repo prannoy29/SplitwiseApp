686b87264abd39519694cd5e40196b7abce7ecff
Atul Anand <atul.anand@zemosolabs.com>
2017-08-26 15:28:45 +0530

    UserId and GroupId rectified for GET Requests

--- src/main/java/demosplitwise/demo/repositories/GroupRepository.java
+++ src/main/java/demosplitwise/demo/repositories/GroupRepository.java
@@ -1,7 +1,6 @@
 package demosplitwise.demo.repositories;
 
 import demosplitwise.demo.domain.Group;
-import demosplitwise.demo.domain.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
